5e31735176ab81e31e2105d6b8410d6489a3ecac
Rodrigo <rodrigocosta47@outlook.com>
2025-05-10 21:25:51 -0300

    Primeiro execicio - tow pointer

---
+++ 01_tow_pointer.py
@@ -0,0 +1,25 @@
+class Solution:
+    
+    def reverseWord_manual(word):
+        res = ''
+        left, rigth = 0, 0
+        
+        while rigth < len(word):
+            if word[rigth] != ' ':
+                rigth += 1
+            else:
+                res += word[left:rigth+1][::-1]
+                rigth += 1
+                left = rigth
+            
+        #print("string antes da ultima palavra-----",res)
+        res += ' '
+        res += word[left:rigth + 2][::-1]               
+        #print("string depois da ultima palavra-----",res)
+        res_trim = res[1:]
+        
+        return res_trim, len(res_trim)
+
+
+palavra_invertida, quant = Solution.reverseWord_manual("arvalap oremun mu ed")
+print(f"Palavra invertida = {palavra_invertida}\nLetas = {quant}")
